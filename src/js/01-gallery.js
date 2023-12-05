cc8f2f982c0d99a8f2acfd5876561d3e6c7ab6c5
EvheniiDovhui <edovguy97@gmail.com>
2023-12-05 22:37:24 +0200

    HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,50 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(hello);
-
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const divRef = document.querySelector('.gallery');
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+divRef.innerHTML = `<ul class="gallery">${addGalleryMarkup}</ul>`;
+
+function onImgClick(evt) {
+  blockStandardAction(evt);
+
+  if (!evt.target.classList.contains('js-gallery-img')) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  divRef.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+function blockStandardAction(evt) {
+  evt.preventDefault();
+}
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+// 03-feedback.js
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const storageKey = 'feedback-form-state';
+
+function saveFormState() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function restoreFormState() {
+  const savedState = localStorage.getItem(storageKey);
+  if (savedState) {
+    const parsedState = JSON.parse(savedState);
+    emailInput.value = parsedState.email || '';
+    messageInput.value = parsedState.message || '';
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+
+  localStorage.removeItem(storageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+form.addEventListener('input', throttle(saveFormState, 500));
+document.addEventListener('DOMContentLoaded', restoreFormState);
+form.addEventListener('submit', handleSubmit);
